a2296769b5767cb07e02685ad0c8596fa5f4f5a2
Jason Yao <r06922002@g.ntu.edu.tw>
2019-12-16 17:59:52 +0800

    Create an empty MFC project

---
+++ MFCMouseMoveAndClick/MFCMouseMoveAndClick.h
@@ -0,0 +1,32 @@
+
+// MFCMouseMoveAndClick.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CMFCMouseMoveAndClickApp:
+// See MFCMouseMoveAndClick.cpp for the implementation of this class
+//
+
+class CMFCMouseMoveAndClickApp : public CWinApp
+{
+public:
+	CMFCMouseMoveAndClickApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCMouseMoveAndClickApp theApp;
